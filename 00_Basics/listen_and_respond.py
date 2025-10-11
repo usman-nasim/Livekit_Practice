ccc59a5a2dd06baa9c667bea5bfee3eabc0e11a9
Usman Nasim <hafiz.usman.nasim.001@gmail.com>
2025-10-12 01:24:38 +0500

    listen and respond bug fixes and working now

--- 00_Basics/listen_and_respond.py
+++ 00_Basics/listen_and_respond.py
@@ -20,7 +20,7 @@ from livekit.agents.voice import Agent, AgentSession
 from livekit.plugins import cartesia, deepgram, noise_cancellation, silero, google
 from livekit.plugins.turn_detector.multilingual import MultilingualModel
 
-load_dotenv(dotenv_path=Path(__file__).parent.parent.parent / ".env.local")
+load_dotenv(dotenv_path=Path(__file__).parent.parent / ".env")
 
 # print(os.getenv("GOOGLE_API_KEY"))
 # add more parents to read
@@ -28,7 +28,7 @@ load_dotenv(dotenv_path=Path(__file__).parent.parent.parent / ".env.local")
 
 class ListenAndRespondAgent(Agent):
     def __init__(self)->None:
-        super().__init_(
+        super().__init__(
             instructions="You are a helpful assistant that listens to the user and responds appropriately.",
         )
         
@@ -38,18 +38,21 @@ class ListenAndRespondAgent(Agent):
 
 async def entrypoint(ctx: JobContext):
     session = AgentSession(
-        agent = ListenAndRespondAgent(),
+        #agent = ListenAndRespondAgent(),
         stt = deepgram.STT(model="nova-3", language="multi"),
         llm=google.LLM(model="gemini-2.0-flash"),
         tts=cartesia.TTS(voice="6f84f4b8-58a2-430c-8c79-688dad597532"),
-        turn_detection=MultilingualModel(),
-        vad=ctx.proc.userdata["vad"],
+        #turn_detection=MultilingualModel(),
+        vad=silero.VAD.load()
+
      )   
         
     await session.start(
-        #agent=ListenAndRespondAgent(),
+        agent=ListenAndRespondAgent(),
         room=ctx.room
     )
             
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
+
+# fixed the __init__
