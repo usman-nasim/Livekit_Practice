65341b83ce946c880c2a42539fbb81ee38e05571
Falcon-Emperor <hafiz.usman.nasim.001@gmail.com>
2025-10-14 17:57:05 +0500

    say phrase in voice completed

--- 00_Basics/09_say_in_voice.py
+++ 00_Basics/09_say_in_voice.py
@@ -20,3 +20,46 @@ from livekit.agents import JobContext, WorkerOptions, cli
 from livekit.agents.llm import function_tool
 from livekit.agents.voice import Agent, AgentSession
 from livekit.plugins import silero, inworld, deepgram, google, cartesia
+
+load_dotenv(dotenv_path=Path(__file__).parent.parent / '.env')
+
+logger = logging.getLogger("say-in-voice")
+logger.setLevel(logging.INFO)
+
+class SayPhraseInVoiceAgent(Agent):
+    def __init__(self) -> None:
+        super().__init__(
+            instructions="""
+                You are an agent that can say phrases in different voices.
+            """,
+            stt=deepgram.STT(model="nova-3", language="multi"),
+            llm=google.LLM(model="gemini-2.0-flash"),
+            tts=inworld.TTS(voice="Ashley"),
+            vad=silero.VAD.load()
+        )
+
+    async def say_phrase_in_voice(self, phrase, voice="Hades"):
+        self.tts.update_options(voice=voice)
+        await self.session.say(phrase)
+        self.tts.update_options(voice="Ashley")
+
+    @function_tool
+    async def say_phrase_in_voice_tool(self, phrase: str, voice: str = "Ashley"):
+        """Say a phrase in a specific voice"""
+        await self.say_phrase_in_voice(phrase, voice)
+
+    async def on_enter(self):
+        self.session.generate_reply()
+
+async def entrypoint(ctx: JobContext):
+    await ctx.connect()
+
+    session = AgentSession()
+
+    await session.start(
+        agent=SayPhraseInVoiceAgent(),
+        room=ctx.room
+    )
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
